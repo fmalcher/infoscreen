1a579df2244b4de787af0404b055ef7662f601ec
fmalcher <fmalcher@v2201212147879901.yourvserver.net>
2014-01-24 22:27:11 +0100

    Added simple class for API handling

---
+++ StationMonitor.class.php
@@ -0,0 +1,50 @@
+<?php
+
+class StationMonitor{
+	private $data = array();
+	private $stationID;
+	
+	public __construct($stationID){
+		$this->stationID = $stationID;
+		$useragent = "easy.GO Client Android v4.0.3_easyGO_4.0.7 Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36;";
+		$requestURI = "http://hn1.the-agent-factory.de/easygo2/rest/regions/MDV/modules/stationmonitor?con10=1&con01=1&sm10=0&sm01=0&source=HISTORY&cStyle=0&transportFilter=00011111&hafasID=".$stationID."&mode=DEP&sm01=0&sm10=0&con01=1&con10=1&cStyle=0";
+	
+		$clientID = $this->createClientID();
+		
+		
+		
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+	    	CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $requestURI,
+			CURLOPT_USERAGENT => $useragent,
+			CURLOPT_HTTPHEADER => array("EasyGO-Client-ID: 61173728391672902674536", "Accept: application/json"),
+		));
+		$resp = curl_exec($curl);
+		curl_close($curl);
+		$this->data = json_decode($resp);
+	}
+	
+	public getJourneys(){
+		return $this->data->journeys;
+	}
+	
+	public getStationName(){
+		return $this->data->stationName;
+	}
+	
+	
+	private createClientID(){
+		$out = null;
+		
+		for($i = 0; $i < 23; $i++{
+			$out .= rand(0,9);
+		}
+		
+		return $out;
+	}
+	
+	
+}
+
+?>
