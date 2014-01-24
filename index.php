d5b2767c26cf33757552f2695e27f3f483089c94
fmalcher <mail@ferdinand-malcher.de>
2014-01-24 20:23:34 +0100

    Random Client ID

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => 'http://hn1.the-agent-factory.de/easygo2/rest/regions/MDV/modules/stationmonitor?con10=1&con01=1&sm10=0&sm01=0&source=HISTORY&cStyle=0&transportFilter=00011111&hafasID=11330&mode=DEP&sm01=0&sm10=0&con01=1&con10=1&cStyle=0',
     CURLOPT_USERAGENT => 'easy.GO Client Android v4.0.3_easyGO_4.0.7 Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36',
-    CURLOPT_HTTPHEADER => array("EasyGO-Client-ID: 62796187481377194541492","Accept: application/json"),
+    CURLOPT_HTTPHEADER => array("EasyGO-Client-ID: 61173728391672902674536","Accept: application/json"),
 ));
 $resp = curl_exec($curl);
 curl_close($curl);
