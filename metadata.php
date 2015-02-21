211a8d6bf5328421e891b0a6846e4c3510cdac47
Wolf <vv01f@users.noreply.github.com>
2015-02-21 15:07:41 +0100

    Update metadata.php
    hier auch mla curl weil bereits an andrer stelle verwendet und angeblich die bessere Lösung...

--- metadata.php
+++ metadata.php
@@ -1,19 +1,45 @@
 <?php
+
 require_once("Config.class.php");
 Config::init();
 
-$get = $_GET['get'];
+//könnte auch $argv für cli unterstützen
 
-if($get == "temp"){
-	$loc   = explode(",", Config::$pref['weather_location']);
-	$wdata = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?lang=de&units=metric&lat=".$loc[0]."&lon=".$loc[1]));
+//$get = $_GET['get'];
+
+//noch unnütz, wenn dann switch ggf. sinnvoller
+//if($get == 'temp'){
+	$loc   = explode(',', Config::$pref['weather_location']);
+	$url="http://api.openweathermap.org/data/2.5/weather?lang=de&units=metric&lat=".$loc[0]."&lon=".$loc[1];
+
+
+// StationMonitor nutzt schon curl, daher hier als besserer Ersatz für file_get_contents
+if ( !function_exists('curl_init')
+	|| !function_exists('curl_setopt')
+	|| !function_exists('curl_exec')
+){
+	echo "Fehler: php_curl geht nicht!";//Fehlermeldungen ersparen Nachfragen und manuelle Suche
+}else{
+	$curl_handle=curl_init();
+	curl_setopt($curl_handle, CURLOPT_URL,$url);
+	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl_handle, CURLOPT_USERAGENT, 'infoscreen');
+	$query = curl_exec($curl_handle);
+	curl_close($curl_handle);
+
+	$wdata = json_decode($query /*file_get_contents($url)*/ );
 	$temp = $wdata->main->temp;
 
-	$temp = round($temp, 1); //one digit after separator
-	$temp = str_replace(".", ",", $temp); //comma as separator
-	$temp .= "&deg;C"; //add �C
-	
+	if($temp != ""){
+		$temp = round($temp, 1); //one digit after separator
+		$temp = str_replace(".", ",", $temp); //comma as separator
+		$temp .= "&deg;C"; //add ¡C
+	}else{
+		$temp = 'Fehler: Unerwartetes Resultat ist "'.$query.'"';
+	}
 	echo $temp;
 }
+//}
 
 ?>
